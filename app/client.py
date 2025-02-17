84e504402ab09fbe703a15e30d5be193d8e1b8b4
ken-ndj <mouakenji8@gmail.com>
2025-02-17 10:04:40 +0100

    serveur client connexion

--- app/client.py
+++ app/client.py
@@ -1,1 +1,6 @@
 import socket # Gère les connexions par socket 
+
+connexion_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Crée un socket pour se connecter à un serveur
+connexion_serveur.connect(('localhost', 1234)) # Se connecte au serveur localhost:1234  
+print('Connecté au serveur localhost:1234')
+connexion_serveur.close() # Ferme la connexion
--- app/serveur.py
+++ app/serveur.py
@@ -1,1 +1,12 @@
 import socket # Gère les connexions par socket
+
+socket_ecoute = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Crée un socket pour écouter les connexions
+socket_ecoute.bind(('localhost', 1234)) # Associe le socket à l'adresse localhost:1234
+socket_ecoute.listen(5) # Commence à écouter les connexions entrantes avec 5 connexions en attente maximum
+print('Serveur démarré sur localhost:1234')
+
+connexion_client, adresse_client = socket_ecoute.accept() # Accepte une connexion entrante
+print('Connexion reçue de', adresse_client) # Affiche l'adresse de l'expéditeur de la connexion 
+
+connexion_client.close()
+socket_ecoute.close() # Ferme le socket d'écoute
