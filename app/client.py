0b75f43a90c859cb7ad74324da56efe1333acc34
gauthy66 <gauthy.dussart66@gmail.com>
2025-02-17 10:32:41 +0100

    update send() to sendall()

--- app/client.py
+++ app/client.py
@@ -3,8 +3,7 @@ import socket # Gère les connexions par socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connexion_serveur: # Crée un socket pour écouter les connexions
     connexion_serveur.connect(('localhost', 1234)) # Se connecte au serveur localhost:1234
     print('Connecté au serveur localhost:1234') # Affiche un message de confirmation
-    connexion_serveur.send(b'Bonjour serveur !') # Ce que le client envoie au serveur
+    connexion_serveur.sendall(b'Bonjour serveur !') # Ce que le client envoie au serveur
     print('Message envoyé au serveur') # Affiche un message de confirmation
-    
     
 connexion_serveur.close() # Ferme la connexion
