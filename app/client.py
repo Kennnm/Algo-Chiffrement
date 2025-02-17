3dd3b26b69c6ae134295b6f93d5a26ffee96e0cc
ken-ndj <mouakenji8@gmail.com>
2025-02-17 10:31:54 +0100

    correction ptit pb

--- app/client.py
+++ app/client.py
@@ -1,9 +1,10 @@
 import socket # Gère les connexions par socket 
 
-connexion_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Crée un socket pour se connecter à un serveur
-connexion_serveur.connect(('localhost', 1234)) # Se connecte au serveur localhost:1234  
-print('Connecté au serveur localhost:1234')
-
-connexion_serveur.send(b'Bonjour serveur !') # Ce que le client envoie au serveur
-
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connexion_serveur: # Crée un socket pour écouter les connexions
+    connexion_serveur.connect(('localhost', 1234)) # Se connecte au serveur localhost:1234
+    print('Connecté au serveur localhost:1234') # Affiche un message de confirmation
+    connexion_serveur.send(b'Bonjour serveur !') # Ce que le client envoie au serveur
+    print('Message envoyé au serveur') # Affiche un message de confirmation
+    
+    
 connexion_serveur.close() # Ferme la connexion
