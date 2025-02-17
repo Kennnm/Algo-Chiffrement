a5864882980b09c30f98e4ba2f35d6ab2551412c
ken-ndj <mouakenji8@gmail.com>
2025-02-17 10:23:51 +0100

    amelioration with ... as
    permet d'eviter les eventuelles erreurs de connexion

--- app/serveur.py
+++ app/serveur.py
@@ -1,7 +1,12 @@
 import socket # Gère les connexions par socket
 
+nom_hote = 'localhost' # Nom d'hôte du serveur
+port_ecoute = 1234 # Port d'écoute du serveur
+
+    
 socket_ecoute = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Crée un socket pour écouter les connexions
-socket_ecoute.bind(('localhost', 1234)) # Associe le socket à l'adresse localhost:1234
+
+socket_ecoute.bind((nom_hote, port_ecoute)) # Associe le socket à l'adresse localhost:1234
 socket_ecoute.listen(5) # Commence à écouter les connexions entrantes avec 5 connexions en attente maximum
 print('Serveur démarré sur localhost:1234')
 
