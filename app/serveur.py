c46778f4bb96bd1dbb386450142d52970a5d26ee
gauthy66 <gauthy.dussart66@gmail.com>
2025-02-19 10:46:55 +0100

    add vigenere

--- app/serveur.py
+++ app/serveur.py
@@ -4,15 +4,24 @@ import json
 
 clients = {}  # Utilisation d'un dictionnaire pour stocker les clients
 
+def vigenere_encrypt(plaintext, key):
+    encrypted_text = []
+    key_length = len(key)
+    key_as_int = [ord(i) for i in key]
+    plaintext_int = [ord(i) for i in plaintext]
+    for i in range(len(plaintext_int)):
+        value = (plaintext_int[i] + key_as_int[i % key_length]) % 26
+        encrypted_text.append(chr(value + 65))
+    return ''.join(encrypted_text)
+
 async def handle_client(websocket):
-    
     # Recevoir le nom d'utilisateur et l'ajouter au dictionnaire avec le WebSocket comme valeur
     username = await websocket.recv()
     login_data = json.loads(username)
     username = login_data["username"]
     clients[username] = websocket  # Ajouter le client avec son nom d'utilisateur comme clé
     print(f"({username}) s'est connecté")
-    
+
     # Demander au client de choisir une option
     message = await websocket.recv()
     choix_user_menu = json.loads(message)
@@ -23,21 +32,25 @@ async def handle_client(websocket):
 
     try:
         async for message in websocket:
-            print(f"Message reçu de {username}: {message}")
-            data = json.loads(message)
-            print(data)
             
+            data = json.loads(message)
             if data["type"] == "ready":
                 print(f"{username} est prêt")
                 continue
             
+            print(data)
+            
             if data["type"] == "message_prive":
+
+                encrypted_data = vigenere_encrypt(data, "clé")  # Chiffrer le message
+                print(f"Message reçu de {username}: {message}")
+
                 destinataire = data["destinataire"]
                 if destinataire in clients:
                     data = {"type": "message_prive", "expediteur": username, "message": data["message"]}
-                    await clients[destinataire].send(json.dumps(data))
-                elif data["type"] == "erreur":
-                    error = {"type": "erreur", "message": f"Destinataire {data["destinataire"]} non trouvé."}
+                    await clients[destinataire].send(json.dumps(encrypted_data))
+                else:
+                    error = {"type": "erreur", "message": f"Destinataire {data['destinataire']} non trouvé."}
                     await websocket.send(json.dumps(error))
             else:
                 error = {"type": "erreur", "message": "Commande inconnue."}
